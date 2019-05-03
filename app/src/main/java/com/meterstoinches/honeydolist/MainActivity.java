8bc13f5388e5864a3a9c91a54890867a432ac769
Dinesh Nanda <dannynanda76@gmail.com>
2019-05-03 14:22:06 -0400

    Initial commit

---
+++ app/src/main/java/com/meterstoinches/honeydolist/MainActivity.java
@@ -0,0 +1,87 @@
+package com.meterstoinches.honeydolist;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    private Button savebtn;
+    private EditText entermsg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        savebtn = (Button) findViewById(R.id.savebtn);
+        entermsg = (EditText) findViewById(R.id.editText);
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!entermsg.getText().toString().equals("")){
+                    String message = entermsg.getText().toString();
+                    writeToFile(message);
+                }else {
+                    //do nothing for now
+                }
+            }
+        });
+        try {
+            if(readFromFile()!=null){
+
+                entermsg.setText(readFromFile());
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToFile(String message){
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("todolist.txt",
+                    Context.MODE_PRIVATE));
+            outputStreamWriter.write(message);
+            outputStreamWriter.close(); //always close your streams!!!
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFromFile() throws IOException {
+
+        String result = "";
+        InputStream inputStream = openFileInput("todolist.txt");
+
+        if (inputStream != null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String tempString = "";
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((tempString=bufferedReader.readLine())!=null){
+
+                stringBuilder.append(tempString);
+            }
+            inputStream.close();
+
+            result=stringBuilder.toString();
+        }
+
+        return result;
+    }
+}
